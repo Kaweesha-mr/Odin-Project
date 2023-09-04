e38e063a55899e550f006b0aed23fdf0d3114202
kaweesha-mr <kaweesha.mr@gmail.com>
2023-09-04 20:48:24 +0530

    able to handle few dialog features

---
+++ JavaScript-Mini-Library/script.js
@@ -0,0 +1,13 @@
+
+
+const addBook = document.querySelector('.add-book');
+const cancel = document.querySelector('.Cancel-form');
+const dialog = document.getElementById('enter-book');
+const showButton = document.getElementById('showDialog');
+
+dialog.showModal();
+
+showButton.addEventListener('click', () => {
+  
+});
+
