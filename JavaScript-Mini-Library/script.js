cee4b71628573dbfdcfaf8d860a589cc7a2c3978
kaweesha-mr <kaweesha.mr@gmail.com>
2023-09-07 12:59:28 +0530

    backdrop features learned

--- JavaScript-Mini-Library/script.js
+++ JavaScript-Mini-Library/script.js
@@ -1,15 +1,12 @@
 
 
-const addBook = document.querySelector('.add-book');
+const addBook = document.querySelector('.btn');
 const cancel = document.querySelector('.Cancel-form');
 const dialog = document.getElementById('enter-book');
 const showButton = document.getElementById('showDialog');
 
 
-
-
-
-
 addBook.addEventListener('click', () => {
     dialog.showModal();
 });
+
